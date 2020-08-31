117c695d24f16e24eb246b990bafc91f787aff0d
jinling <luoage@msn.cn>
2020-08-31 19:37:28 +0800

    add feature

--- index.js
+++ index.js
@@ -2,9 +2,24 @@
 
 const FS     = require('fs')
 const globby = require('globby')
-const r1     = /^(let|var|const) +([a-zA-Z_$][a-zA-Z0-9_$]*) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)/gm // const createStore = require('redux')
-const r2     = /^(let|var|const) +([a-zA-Z_$][a-zA-Z0-9_$]*) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)\.([a-zA-Z][a-zA-Z0-9]+)/gm // const createStore = require('redux').createStore
-const r3     = /^(let|var|const) +(\{\s*([a-zA-Z_$][a-zA-Z0-9_$]*)\s*\}) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)/gm // const { createStore } = require('redux')
+const path = require('path')
+
+// const createStore = require('redux')
+const r1     = /^(let|var|const)\s+(\S+)\s+\=\s+require\((\S+)\)/gm
+// const createStore = require('redux').createStore
+const r2     = /^(let|var|const)\s+(\S+)\s+\=\s+require\((\S+)\)\.(\w+)/gm
+// const { createStore } = require('redux')
+const r3     = /^(let|var|const)\s+\{([0-9a-zA-Z-,_\s]+)\}\s+\=\s+require\((\S+)\)/gm
+// const logger = require('log4js').getLogger('controller.user')
+const r4     = /^(let|var|const)\s+(\w+)\s+\=\s+require\((\S+)\)\.(\w+)\((\S+)*\)/gm
+// exports.name =
+const r5 = /^(module\.)*exports\.(\w+)\s*=\s*/gm
+// module.exports = 
+const r6 = /^module.exports(\.__proto__)*\s*=\s*/gm
+// require('../')
+const r7 = /^require\(([^\(]+)\)([\s;]*)$/gm
+// __filename
+const r8 = /^(.*require.*\b)__filename\b/m
 
 const args = process.argv.slice(2)
 
@@ -13,19 +28,35 @@ if (!args.length) {
   process.exit(1)
 }
 
+const ignores = ['node_modules']
 const paths = globby.sync(args)
 
 paths.forEach(function (p) {
-  if (!FS.statSync(p).isDirectory()) {
-    return replaceInFile(p)
-  }
+  return replaceInFile(p)
 })
 
 function replaceInFile(fp) {
+  if (ignores.includes(path.basename(fp))) {
+    return
+  }
+  if (FS.statSync(fp).isDirectory()) {
+    globby.sync(`${fp}/*`).forEach((p1) => {
+      return replaceInFile(p1)
+    })
+  }
+  if (!/^\.m*js$/.test(path.extname(fp))) {
+    return
+  }
   const result = FS.writeFileSync(fp, FS.readFileSync(fp, 'utf-8')
-    .replace(r3, `import { $3 } from $5`)
-    .replace(r2, `import { $7 as $2 } from $4`)
-    .replace(r1, `import $2 from $4`), 'utf-8')
+    .replace(r8, `import { fileURLToPath } from 'url';\nconst __filename = fileURLToPath(import.meta.url);\n$1__filename`)
+    .replace(r6, `export default `)
+    .replace(r5, `export const $2 = `)
+    .replace(r4, `import _$2 from $3;\nconst $2 = _$2.$4($5)`)
+    .replace(r3, `import {$2} from $3`)
+    .replace(r2, `import { $4 as $2 } from $3`)
+    .replace(r1, `import $2 from $3`)
+    .replace(r7, `import $1$2`)
+    , 'utf-8')
   console.log(`> ${fp}`)
   return result
 }
--- test-input.js
+++ test-input.js
@@ -1,9 +1,30 @@
 const createStore0 = require('redux')
 const createStore1 = require('redux').createStore
 const { createStore2 } = require('redux')
+const { createStore3 } = require('redux');
+const {
+  createStore4
+} = require('redux');
+const {
+  createStore5,
+  createStore6,
+} = require('redux');
 
-console.dir({
-  createStore0,
-  createStore1,
-  createStore2
-})
+var aa1 = require('./aa1');
+let aa2 = require('../../aa2');
+const logger1 = require('log4js').getLogger('controller.user');
+const logger2 = require('log4js').getLogger();
+const logger3 = require('log4js').getLogger(__filename);
+const logger4 = require('log4js').getLogger(__filename);
+require('../../test');
+
+exports.name1 = function(n) {
+  return n;
+};
+
+module.exports.name2 = function(n) {
+};
+
+module.exports = {};
+
+module.exports.__proto__ = {};
